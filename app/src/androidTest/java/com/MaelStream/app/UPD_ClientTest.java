3f49db868c45f70fd07dd46c0c7bd4ad75a2f350
KarenMission <Converse.123>
2020-03-30 17:13:49 -0700

    Acceptance Tests Added

--- app/src/androidTest/java/com/MaelStream/app/UPD_ClientTest.java
+++ app/src/androidTest/java/com/MaelStream/app/UPD_ClientTest.java
@@ -17,24 +17,57 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class UPD_ClientTest {
+
+    // Incorrect Inputs for Address and Port Test
+    private static final String emptyAddress = "";
+    private static final String emptyPort = "";
+
+    private static final String incorrectAddress = "192.168.1.70";
+    private static final String incorrectPort = "4424";
+
+    private static final String formatAddress = "192168174";
+    private static final String formatPort = "wasde";
+
+    // Acceptable Inputs for Address and Port Test
+    private static final String correctAddress = "192.168.1.74";
+    private static final String correctPort = "4618"
+
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
     /**
      * Desc:
-     *
+     * Acceptance Testing to the ensure the app is working as intended
      *
      * Bugs:
      *  None atm
      */
     @Test
     public void streamWorking() throws Exception {
-        // Type in IP address and port number in use
-        onView(withId(R.id.address)).perform(typeText("192.168.1.74"), closeSoftKeyboard());
-        onView(withId(R.id.port)).perform(typeText("10080"), closeSoftKeyboard());
+        // Test for validity of incorrect inputs
+        onView(withId(R.id.address)).perform(typeText(emptyAddress), closeSoftKeyboard());
+        onView(withId(R.id.port)).perform(typeText(emptyPort), closeSoftKeyboard());
+        onView(withId(R.id.connect)).perform(click());
+
+        onView(withId(R.id.address)).perform(typeText(formatAddress), closeSoftKeyboard());
+        onView(withId(R.id.port)).perform(typeText(formatPort), closeSoftKeyboard());
+        onView(withId(R.id.connect)).perform(click());
+
+        onView(withId(R.id.address)).perform(typeText(incorrectAddress), closeSoftKeyboard());
+        onView(withId(R.id.port)).perform(typeText(incorrectPort), closeSoftKeyboard());
+        onView(withId(R.id.connect)).perform(click());
+
+        // Type in valid IP address and port number
+        onView(withId(R.id.address)).perform(typeText(correctAddress), closeSoftKeyboard());
+        onView(withId(R.id.port)).perform(typeText(correctPort), closeSoftKeyboard());
 
         // Connect to the video
         onView(withId(R.id.connect)).perform(click());
+
+        // Toggle the grey button
+        onView(withId(R.id.toggleButton)).perform(click());
+        onView(withId(R.id.toggleButton)).perform(click());
+        onView(withId(R.id.toggleButton)).perform(click());
 
         //Close screen
         onView(withId(R.id.btnReturn)).perform(click());
